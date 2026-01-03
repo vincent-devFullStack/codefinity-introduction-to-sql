e1d3c7ad0b9e29e34078f467877111456bb735f1
vincent-devFullStack <>
2026-01-03 12:49:15 +0000

    AND operator

---
+++ advanced_data_filtering/and_operator/query.sql
@@ -0,0 +1,5 @@
+-- Write a SQL query to select only the names of countries from Asia with a population greater than 1000000
+SELECT name
+FROM country
+WHERE continent = 'Asia' AND population > 10000000
+
